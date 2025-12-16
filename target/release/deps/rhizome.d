b5174e6c10f995912035513899b0c6b563e8ecfd
Kostya Betenya <betenya.kostya@mail.ru>
2025-12-16 16:18:08 +0300

    feat: create ReadMe.md

--- target/release/deps/rhizome.d
+++
@@ -1,5 +0,0 @@
-/Users/kostyabet/GitHub/vazon/rhizome/target/release/deps/rhizome.d: src/lib.rs
-
-/Users/kostyabet/GitHub/vazon/rhizome/target/release/deps/librhizome.dylib: src/lib.rs
-
-src/lib.rs:
--- target/release/librhizome.d
+++
@@ -1,1 +0,0 @@
-/Users/kostyabet/GitHub/vazon/rhizome/target/release/librhizome.dylib: /Users/kostyabet/GitHub/vazon/rhizome/src/lib.rs
