860cb82668772a6fda5369c02a7a8705703a2b5b
Kostya Betenya <betenya.kostya@mail.ru>
2026-01-13 13:40:53 +0300

    feat: create doc for dht protocol

--- src/dht/mod.rs
+++ src/dht/mod.rs
@@ -1,8 +1,13 @@
 /// Basic description of node in Kademlia DHT
 ///
+/// Describe `Who`
+///
 /// Contain two modules:
 /// - `NodeId` - uniq identifier in Kademlia DHT network
 /// - `Node` - implementation of Kademlia DHT node with state, last seen and TTL
 pub mod node;
+/// Realization of Kademlia Work
+///
+/// Describe `How`
 pub mod protocol;
 pub mod routing_table;
