0cd6b28099ebd57e091c96c1b5a78d2172c67329
Kostya Betenya <betenya.kostya@mail.ru>
2026-01-13 15:59:14 +0300

    feat: create documentation for network protocol

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,2 +1,11 @@
+/// Consts for each type of message
+///
+/// Need for serialization in network.
+pub mod consts;
+/// Network protocol
+///
+/// Module for sending data and receive data from internet.
+/// They will transfer abstract command like (Ping, Store) in real bytes and send it by UDP.
+/// Protocol work with answers and responsibility for safety.
 pub mod protocol;
 pub mod transport;
