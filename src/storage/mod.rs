8afd7778194396bd831425754a7ece2e9889ee0f
Kostya Betenya <betenya.kostya@mail.ru>
2026-01-14 10:58:06 +0300

    feat: create documentation for storage keys

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,5 +3,8 @@
 /// They convert bytes in to the rust object for using in work.
 /// Also, this module can describe the style of content in threads and messages of the network.
 pub mod data_types;
+/// This module standardize the keys in network
+///
+/// It means that by this module anyone can use thread id and choose one uniq hash for data
 pub mod keys;
 pub mod main;
